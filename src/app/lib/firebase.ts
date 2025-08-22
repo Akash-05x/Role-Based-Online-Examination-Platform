edc9182a4f9edb346fc03679edf153858968c861
Akash-05x <akashcrazy2004@gmail.com>
2025-08-22 19:07:41 +0530

    Update firebase.ts

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9ZNd1HrZYTfzYKTl3hykICKPs2_8bPPk",
-  authDomain: "mini-39bc3.firebaseapp.com",
-  projectId: "mini-39bc3",
-  storageBucket: "mini-39bc3.appspot.com",
-  messagingSenderId: "69321091430",
-  appId: "1:69321091430:web:b99af8ff509c075c3e80a7",
-  measurementId: "G-CB01HJB188",
+  apiKey: "Your firebase api key",
+  authDomain: "...",
+  projectId: "project id",
+  storageBucket: "...",
+  messagingSenderId: "msg sender id",
+  appId: " your app id",
+  measurementId: "measurement id",
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app); 
+
